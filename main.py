d6d0ddd78f22c84f940f09adb9382c2b8c2f485b
akxaalex16 <akxaalex16@gmail.com>
2024-08-05 19:58:33 -0700

    added view and add functions

---
+++ main.py
@@ -0,0 +1,23 @@
+pwd = input('')
+
+
+def view():
+    pass
+
+
+def add():
+    pass
+
+
+while True:
+    mode = input('Would you like to add a new password or view existing ones (view/add)? or q to quit. ').lower()
+    if mode == 'q':
+        break
+
+    if mode == 'view':
+        pass
+    elif mode == 'add':
+        pass
+    else:
+        print('Invalid mode.')
+        continue
