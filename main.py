a90bae968dd497ceb0ea3c4c7b34086fd08b7fd8
akxaalex16 <akxaalex16@gmail.com>
2024-08-06 17:16:30 -0700

    encrypted/decrypted pwd

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-pwd = input('')
+from cryptography.fernet import Fernet
+
+
+'''def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)'''
+
+
+# write_key()
+
+
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
 
 
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print('User:', user, '| Password:', fer.decrypt(passw.encode()).decode())
 
 
 def add():
-    pass
+    name = input('Account Name: ')
+    pwd = input('Password: ')
+
+    # w = write
+    # r = read
+    # a = append
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 
 while True:
@@ -15,9 +46,9 @@ while True:
         break
 
     if mode == 'view':
-        pass
+        view()
     elif mode == 'add':
-        pass
+        add()
     else:
         print('Invalid mode.')
         continue
